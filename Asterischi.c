1ea7d63dd1da4b58f71e52ba0328599d7bee618c
Angelo Caravella <sannita6570@gmailcom>
2018-05-21 13:40:13 +0200

    !

--- Asterischi.c
+++ Asterischi.c
@@ -2,7 +2,7 @@
 #include <time.h>
 int main(){
   struct timespec intervallo;
-  intervallo.tv_sec = 1 ;
+  intervallo.tv_sec = 0 ;
   intervallo.tv_nsec = 700 * 1000 * 1000 ;
   puts ("*");
   nanosleep(&intervallo , NULL);
